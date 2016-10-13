0b8a23974c951b67ff48948b0808d3d48c5f417f
Hendrik Mäeste <Hendrik Meste@A17>
2016-10-13 15:45:54 +0300

    Mod functionality added.

--- src/VariableClass.java
+++ src/VariableClass.java
@@ -10,7 +10,7 @@ public class VariableClass {
 		int d = a / b;
 		
 //				2. Proovida “jäägiga jagamist” %-märgi abil. Kas oskab if-lauset kasutada võib muutuja kohta välja küsida, kas tegemist on paaris-või paarituarvuga.
-
+		int jaak = a % b;
 //				3. Teha muutuja, mis hoiab endas tõeväärtust arvu kohta, kas tegemist on paarisarvuga.
 
 //				4. Kirjutada tingimuslausesse või muutujasse loogika, mis otsustaks, kas arv on suurem kui 10.
