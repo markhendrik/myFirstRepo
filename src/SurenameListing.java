e4f291c127aed38801660bfd9ff7d4bdb794aadf
Hendrik Mäeste <Hendrik Meste@172.30.12.22>
2017-06-09 12:04:47 +0300

    addad

--- src/SurenameListing.java
+++ src/SurenameListing.java
@@ -3,20 +3,39 @@ import java.util.*;
 public class SurenameListing {
 
 	public static void main(String[] args) {
-		String[] names = {"Vitali Vildikas", "Vello Põld", "Kirju Marju", "Indrek Kaigas"};
-		System.out.println(getSurenameList(names)); //{"Vildikas", "Põld", "Marju", "Kaigas"}
+		String[] names = { "Vitali Vildikas", "Vello Põld", "Kirju marju", "Indrek Kaigas" };
+		System.out.println(getSurnameList(names)); // {"Vildikas", "Põld",
+													// "Marju", "Kaigas"}
+		System.out.println(isNameUpper("Vitali Vildikas"));
+		System.out.println(seperateSurename("Vello Põld"));
 	}
 
-	//1. Luua meetod, mis eraldab inimese nimest tema perekonnanime. Nimi on nt. Mari Maasikas – > Maasikas
+	// 1. Luua meetod, mis eraldab inimese nimest tema perekonnanime. Nimi on
+	// nt. Mari Maasikas – > Maasikas
 	public static String seperateSurename(String fullname) {
-		return "";
+		return fullname.substring(fullname.indexOf(" ") + 1);
 	}
-	//2. Luua alammeetod, mis kontrollib, kas perekonnanimi algab suure tähega.
-	private boolean isNameUpper(String name) {
-		return false;
+
+	// 2. Luua alammeetod, mis kontrollib, kas perekonnanimi algab suure tähega.
+	private static boolean isNameUpper(String name) {
+		char firstChar = name.charAt(0);
+		return Character.isUpperCase(firstChar);
 	}
-	//3. Etteantud massiivis olevatest nimedest võtta välja kõikide inimeste perekonnanimed ning lisada need listi.
-	public static List<String> getSurenameList(String[] names) {
-		return null;
+
+	// 3. Etteantud massiivis olevatest nimedest võtta välja kõikide inimeste
+	// perekonnanimed ning lisada need listi.
+	public static List<String> getSurnameList(String[] names) {
+		List<String> surnames = new ArrayList<String>();
+		for (String name : names) {
+			String surname = seperateSurename(name);
+			if (isNameUpper(surname)) {
+				surnames.add(surname);
+			} else {
+				String modSurname = surname.substring(0, 1).toUpperCase() + surname.substring(1);
+				surnames.add(modSurname);
+				
+			}
+		}
+		return surnames;
 	}
 }
